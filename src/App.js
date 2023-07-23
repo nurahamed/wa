e682dc43ad59fab7b3f527869ca0c9807f3df632
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-07-23 21:10:55 +0530

    Docs: add serviss and app folder

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { theme } from "./theme/theme";
 import { DashboardContainer, MiddleContainer } from "./theme/styled";
 import { Routes, Route } from "react-router-dom";
 import { Dashboard, Map, Saves } from "./pages/allPages";
+import LeftNav from "./components/LeftNav";
 import TopNav from "./components/TopNav";
 import WeatherDisplay from "./components/WeatherDisplay";
-import LeftNav from "./components/LeftNav";
 import Footer from "./components/Footer";
 
 const App = () => {
